cf45afb12cdf3f1329ecd0559439f8d6852ddd2d
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2023-07-02 11:37:19 +0100

    stable com

--- app/components/HomePage.js
+++ app/components/HomePage.js
@@ -23,7 +23,10 @@ const HomePage = () => {
             <div>
               <h2 className="text-lg font-semibold text-orange-500 uppercase">Hi, Nkem Benjamin Here!</h2>
               <p className="text-5xl font-bold uppercase stack">
-                I&apos;m A <span className="text-orange-400">Web Developer</span>
+                I&apos;m A{" "}
+                <span className="text-orange-400" id="stack">
+                  Web Developer
+                </span>
               </p>
 
               <div className="mt-4">
@@ -47,20 +50,41 @@ const HomePage = () => {
         </section>
 
         <section className="mx-auto w-11/12 md:w-[1024px] section my-40">
-          <div className="grid items-center gap-6" style={{ gridTemplateColumns: "1.5fr 2fr" }}>
+          <div className="grid gap-6" style={{ gridTemplateColumns: "1.5fr 2fr" }}>
             <div className="grid bg-transparent border border-orange-500 rounded-lg h-96 place-content-center">
               <p>Pic Coming soon...</p>
             </div>
             <div>
-              <h3 className="mb-5 text-xl font-bold uppercase">Skills Strength</h3>
-              <div className="p-4 border border-orange-500 rounded-md h-96">
-                <div className="space-y-6">
-                  <div className="grid items-center gap-4">
-                    <p className="font-semibold">HTML/CSS</p>
+              <h3 className="mb-5 text-xl font-bold uppercase">Languages Strength</h3>
+              <div className="p-4 border border-orange-500 rounded-md">
+                <div className="space-y-8">
+                  <div className="grid items-center gap-4" style={{ gridTemplateColumns: "1fr 5fr 1fr" }}>
+                    <p className="font-semibold">English</p>
                     <div className="w-full h-2 overflow-hidden border border-orange-500 rounded-md">
-                      <div className="w-4/5 h-2 duration-200 bg-orange-500 rounded-r-md progress"></div>
+                      <div className="w-full h-2 duration-200 bg-orange-500 rounded-r-md progress"></div>
+                    </div>
+                    <p>100%</p>
+                  </div>
+                  <div className="grid items-center gap-4" style={{ gridTemplateColumns: "1fr 5fr 1fr" }}>
+                    <p className="font-semibold">Igbo</p>
+                    <div className="w-full h-2 overflow-hidden border border-orange-500 rounded-md">
+                      <div className="w-3/5 h-2 duration-200 bg-orange-500 rounded-r-md progress1"></div>
+                    </div>
+                    <p>60%</p>
+                  </div>
+                  <div className="grid items-center gap-4" style={{ gridTemplateColumns: "1fr 5fr 1fr" }}>
+                    <p className="font-semibold">Yoruba</p>
+                    <div className="w-full h-2 overflow-hidden border border-orange-500 rounded-md">
+                      <div className="w-4/5 h-2 duration-200 bg-orange-500 rounded-r-md progress2"></div>
                     </div>
                     <p>80%</p>
+                  </div>
+                  <div className="grid items-center gap-4" style={{ gridTemplateColumns: "1fr 5fr 1fr" }}>
+                    <p className="font-semibold">Spanish</p>
+                    <div className="w-full h-2 overflow-hidden border border-orange-500 rounded-md">
+                      <div className="w-1/3 h-2 duration-200 bg-orange-500 rounded-r-md progress3"></div>
+                    </div>
+                    <p>33%</p>
                   </div>
                 </div>
               </div>
@@ -78,9 +102,16 @@ const HomePage = () => {
               <div className="border border-orange-500 rounded-md w-52 h-52"></div>
               <div className="border border-orange-500 rounded-md w-52 h-52"></div>
             </div>
+
+            <div className="py-5 text-center">
+              <button className="px-3 py-1 text-orange-500 duration-200 border border-orange-500 rounded-md hover:bg-orange-500 hover:text-black">
+                View All Projects
+              </button>
+            </div>
           </div>
         </section>
 
+        <StackAnim />
         <div className="h-96"></div>
       </main>
     </>
--- app/components/StackAnim.js
+++ app/components/StackAnim.js
@@ -3,32 +3,31 @@
 import { useEffect } from "react";
 
 const StackAnim = () => {
-  const skills = [
-    "Frontend Developer 💖",
-    "UI/UX Designer 🎨🖼",
-    "Backend Developer 👩‍💻",
-    "2D Unity Game Developer 🎮🎯",
-  ];
+  const skills = ["Frontend Developer 💖", "UI/UX Designer 🎨", "Backend Developer 👩‍💻", "2D Unity Game Developer 🎮"];
 
   const animControl = () => {
     const spanText = document.querySelector("#stack");
-    let totalTimeForAnimation = 1000;
+    let totalTimeForAnimation = 2000;
     let currentSkillsIndex = 0;
 
-    spanText.textContent = skills[currentSkillsIndex];
     setInterval(() => {
-      if (currentSkillsIndex === skills.length) {
+      spanText.textContent = skills[currentSkillsIndex];
+      if (currentSkillsIndex < skills.length - 1) {
+        currentSkillsIndex++;
+        spanText.textContent = skills[currentSkillsIndex];
+      } else {
         currentSkillsIndex = 0;
-        return;
+        spanText.textContent = skills[currentSkillsIndex];
       }
 
-      currentSkillsIndex++;
-      spanText.textContent = skills[currentSkillsIndex];
+      console.log(currentSkillsIndex);
     }, totalTimeForAnimation);
   };
 
-  useEffect(() => animControl(), []);
-  return null;
+  useEffect(() => {
+    animControl();
+    return undefined;
+  }, []);
 };
 
 export default StackAnim;
