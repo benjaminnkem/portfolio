6e61864f2c0d8215fb696811dda40b821ed743bb
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2023-08-18 07:09:46 +0100

    Update Footer.js

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -3,77 +3,56 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <>
-      <footer className="py-10 border-t border-cyan-500">
+      <footer className="py-10 border-t border-cyan-500 bg-[#0f0f0f6f]">
         <div className="md:max-w-[1024px] w-11/12 mx-auto">
           <div className="grid grid-cols-1 gap-10 md:grid-cols-2">
-            <div className="text-white ">
-              <div className="space-y-5 text-center md:text-start">
-                <div>
-                  <h4 className="text-2xl font-semibold">
-                    Address <i className="ri-home-8-line "></i>
-                  </h4>
-                  <p className="text-lg font-light">FISDES Hostel Palmview, FUNAAB Abeokuta, Ogun State, Nigeria.</p>
-                </div>
-                <div>
-                  <h4 className="text-2xl font-semibold">Fav. Quote</h4>
-                  <p className="text-lg font-light">
-                    <span className="italic">
-                      &quot;Web development is the art of transforming ideas into engaging and functional online
-                      experiences, where creativity meets technology to create a digital masterpiece.&quot;
-                    </span>
-                  </p>
-                </div>
-                {/* <div>
-                  <h4 className="text-2xl font-semibold">Dedication</h4>
-                  <p className="text-lg italic font-light">
-                    &quot;To my remarkable mother, whose unwavering dedication and support have shaped my journey. This
-                    portfolio is a heartfelt tribute to your tireless hard work, love, and guidance. Thank you for
-                    believing in me and inspiring me to reach new heights. I am forever grateful for everything you do.
-                    With boundless love and appreciation, this portfolio is dedicated to you, my incredible mum.&quot;
-                  </p>
-                </div>*/}
-              </div>
+            <div className="mr-auto">
+              <p className="md:text-5xl text-4xl font-extrabold">
+                Ben<span className="text-cyan-500">.</span>
+              </p>
             </div>
-            <div className="text-center text-white md:text-start">
-              <div className="space-y-4">
-                <div>
-                  <h4 className="text-2xl font-semibold">
-                    Links <i className="ri-link-m"></i>
-                  </h4>
-                  <ul className="space-y-2">
-                    <li>
-                      <Link href={"/"}>
-                        <p className="text-lg font-light duration-200 inline-block hover:text-cyan-500">Home</p>
-                      </Link>
-                    </li>
-                    <li>
-                      <Link href={"/contact"}>
-                        <p className="text-lg font-light duration-200 inline-block hover:text-cyan-500">Contact</p>
-                      </Link>
-                    </li>
-                    <li>
-                      <Link href={"/projects"}>
-                        {" "}
-                        <p className="text-lg font-light duration-200 inline-block hover:text-cyan-500">Projects</p>
-                      </Link>
-                    </li>
-                  </ul>
-                </div>
-                <div>
-                  <h4 className="text-2xl font-semibold">Follow Me</h4>
-                  <div className="flex justify-center space-x-4 text-2xl md:justify-start">
-                    <i className="duration-200 hover:text-blue-700 ri-facebook-circle-fill"></i>
-                    <i className="duration-200 hover:text-red-600 ri-instagram-fill"></i>
-                    <i className="duration-200 hover:text-green-500 ri-whatsapp-fill"></i>
-                    <i className="duration-200 ri-twitter-fill hover:text-cyan-600"></i>
-                  </div>
+
+            <div className="text-center text-white md:text-start ml-auto">
+              <div>
+                <ul className="space-x-4 flex">
+                  <li>
+                    <Link href={"/"}>
+                      <p className="duration-200 inline-block hover:text-cyan-500">Home</p>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href={"/contact"}>
+                      <p className="duration-200 inline-block hover:text-cyan-500">Contact</p>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href={"/projects"}>
+                      <p className="duration-200 inline-block hover:text-cyan-500">Projects</p>
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+              <div>
+                <div className="flex justify-center space-x-4 text-2xl md:justify-start">
+                  <Link href={"https://www.linkedin.com/in/benjamin-nkem-23526720b/"} target="_blank">
+                    <i className="cursor-pointer ri-linkedin-line"></i>
+                  </Link>
+                  <Link href={"https://twitter.com/MainNkem"} target="_blank">
+                    <i className="cursor-pointer ri-twitter-fill hover:text-cyan-600"></i>
+                  </Link>
+                  <Link href={"https://github.com/benjaminnkem"} target="_blank">
+                    <i className="cursor-pointer ri-github-fill"></i>
+                  </Link>
+                  <Link href={"https://wa.me/+2348133961439"} target="_blank">
+                  <i className="duration-200 hover:text-green-500 ri-whatsapp-fill"></i>
+                  </Link>
                 </div>
               </div>
             </div>
           </div>
 
-          <div className="pt-10 text-center text-white">
-            <p className="font-light text-center opacity-80">@2023 - Benjamin Nkem</p>
+          <div className="pt-10 text-center">
+            <p className="text-white text-sm font-semibold">@Benjamin Nkem - All Rights Reserved</p>
           </div>
         </div>
       </footer>
