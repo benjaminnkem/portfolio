b76e25d3439c5d3774e5839ae6c09b41eaeb5714
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2024-01-31 22:52:11 +0100

    Update skills.js

--- components/UI/Home/skills.js
+++ components/UI/Home/skills.js
@@ -32,7 +32,7 @@ const Skills = () => {
 
   return (
     <div className="bg-gradient-to-b from-black via-black" id="stats">
-      <section className="min-h-screen relative" id="about" ref={ref}>
+      <section className="relative min-h-screen" id="about" ref={ref}>
         <h3
           className="absolute -top-[15rem] z-[10] -left-5 font-extrabold md:text-[9rem] text-[6rem] text-primaryBlack-200/70"
           id="summary_text"
@@ -82,10 +82,10 @@ const Skills = () => {
                 <p>Skills</p>
               </TransitionOpacityInView>
 
-              <TransitionParentFast addClass="grid grid-cols-8 select-none text-center gap-4">
+              <TransitionParentFast addClass="grid lg:grid-cols-8 md:grid-cols-6 sm:grid-cols-3 grid-cols-2 select-none text-center gap-4">
                 {skills.frontend.map((skill, idx) => (
                   <TransitionOpacity key={idx}>
-                    <div className="duration-200 border py-5 border-primary/10 rounded-md cursor-pointer space-y-1 hover:bg-primary/10">
+                    <div className="py-5 space-y-1 duration-200 border rounded-md cursor-pointer border-primary/10 hover:bg-primary/10">
                       <div className="grid place-content-center">{skill.icon}</div> <p>{skill.label}</p>
                     </div>
                   </TransitionOpacity>
